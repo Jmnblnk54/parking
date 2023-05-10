200307e2698e0c77317f7e11714dbce8d7a60563
jtcup <justin@codechimps.co>
2023-05-10 13:29:45 -0400

    Updated Host login page with new host icon and colors

--- src/view/auth/leftsection/LeftScreen.jsx
+++ src/view/auth/leftsection/LeftScreen.jsx
@@ -8,42 +8,53 @@ export default function IndexPage() {
 
   return (
     <Wrapper>
-      <div className="left-container">
-        <h2>
-          Lets get
-          <span className="logo-btw-heading">
-            <img src={yugoWhite} alt="yugoWhite.svg" />
-          </span>
-          ‘n!
-        </h2>
         {history?.location?.pathname.includes("host") ? (
-          <>
+          <div className="host-bg-icon">
+            <img className="host-icon" src="/static/media/house.5f69030ca3bb055477aeff7736e5d384.svg" alt="" />
+            <div className="left-container-host">
+                <h2>
+                  Lets get
+                  <span className="logo-btw-heading">
+                    <img src={yugoWhite} alt="yugoWhite.svg" />
+                  </span>
+                  ‘n!
+                </h2>
+
             <p>
               Enter your details to start your <br />
               hosting journey with us!
             </p>
             <button
-              className="signup-button"
+              className="signup-button-host"
               onClick={() => history.push("/host/signup")}
             >
               Sign up
             </button>
-          </>
+          </div>
+          </div>
         ) : (
-          <>
-            <p>
-              Enter your details to start reserving <br />
-              your spots ahead of time!
-            </p>
-            <button
-              className="signup-button"
-              onClick={() => history.push("/traveler/signup")}
-            >
-              Sign up
-            </button>
-          </>
+          <div className="left-container">
+          <h2>
+            Lets get
+            <span className="logo-btw-heading">
+              <img src={yugoWhite} alt="yugoWhite.svg" />
+            </span>
+            ‘n!
+          </h2>
+            <div className="traveler-box">
+              <p>
+                Enter your details to start reserving <br />
+                your spots ahead of time!
+              </p>
+              <button
+                className="signup-button"
+                onClick={() => history.push("/traveler/signup")}
+              >
+                Sign up
+              </button>
+            </div>
+          </div>
         )}
-      </div>
     </Wrapper>
   );
 }
--- src/view/auth/leftsection/LeftScreen.styled.jsx
+++ src/view/auth/leftsection/LeftScreen.styled.jsx
@@ -3,15 +3,14 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   height: 100%;
   width: 50%;
-  background: #080f28;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 
-  .left-container {
+  .left-container-host {
+    background: rgba(77,157,116,0.2);
+    height: 100%;
     width: 90%;
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
 
     span {
@@ -33,15 +32,31 @@ export const Wrapper = styled.div`
     }
   }
 
-  .signup-button {
-    background: #4d9d74;
+  .host-bg-icon {
+    height: 100%;
+    z-index: 1;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .host-icon {
+    position: absolute;
+    z-index: -1;
+    height: 600px;
+    left: -140px;
+    bottom: -41px;
+    opacity: 10%;
+  }
+
+  .signup-button-host {
+    background: #080F28;
     padding: 6px 30px;
     width: 60%;
     border: 1px solid #080f28;
     border-radius: 30px;
     color: #fff;
     margin-top: 20px;
-    font-size: 16px;
+    font-size: 1rem;
     cursor: pointer;
   }
 
